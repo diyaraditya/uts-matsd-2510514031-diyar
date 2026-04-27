35f242a31733a51735c7b86eaeb561958ffaebe6
diyaraditya <diyaraditya07@gmail.com>
2026-04-27 16:40:33 +0700

    nambahin identitas dan personalisasi

--- 02_matlab/soal_1_b.m
+++ 02_matlab/soal_1_b.m
@@ -1,3 +1,12 @@
+% =======================================================================
+% UTS Matematika Sains Data Semester Genap 2025/2026
+% Soal 5 Konvergensi CLV dan Kekontinuan Biaya
+%
+% Nama : Muhammad Diyar razaqa Aditya
+% NIM  : 2510514031
+% Parameter: N=31, a=3, b=1, K=5, theta0=95, alpha=0.002
+% Tanggal: 2026-04-25
+% =======================================================================
 D = [27 34 34 41 46 53 53];
 
 mean(D)
